ac276efed3e7b1d20af1a5e0af5daeba22d5c616
Garrett Primm <103901383+garrettprimm@users.noreply.github.com>
2022-04-23 12:37:48 -0500

    initial commit

---
+++ first_ec2.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  shared_credentials_file = "/Users/$USER/.aws/creds"
+}
+
+resource "aws_instance" "my-ec2" {
+  ami = "ami-0c6a6b0e75b2b6ce7"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "my-ec2"
+  }
+}
