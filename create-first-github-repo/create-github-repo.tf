b8d9b348956a1fc58da5bce59e366699d5c56fe5
Garrett Primm <103901383+garrettprimm@users.noreply.github.com>
2022-04-23 14:56:48 -0500

    more cleanup

--- create-first-github-repo/create-github-repo.tf
+++ create-first-github-repo/create-github-repo.tf
@@ -1,3 +1,12 @@
+terraform { #putting all of my required providers here just because. I could consolidate everything into a single file, but I don't want to.
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "github" {} #Use Environment variable for GITHUB_TOKEN
 
 resource "github_repository" "terraform_example" {
